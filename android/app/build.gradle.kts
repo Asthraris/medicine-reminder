2c72f71beef4df6448875dcfc1b148e7527a4280
Aman Gupta <asthraris@gmail.com>
2026-02-04 21:37:46 +0530

    since i configured android build files , upload

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,56 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
+android {
+    namespace = "com.example.medicine_reminder"
+    compileSdk = flutter.compileSdkVersion
+    ndkVersion = flutter.ndkVersion
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+
+        isCoreLibraryDesugaringEnabled = true
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
+    }
+
+    defaultConfig {
+        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
+        applicationId = "com.example.medicine_reminder"
+        // You can update the following values to match your application needs.
+        // For more information, see: https://flutter.dev/to/review-gradle-config.
+        minSdk = flutter.minSdkVersion
+        targetSdk = flutter.targetSdkVersion
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled =true
+            isShrinkResources = true
+            signingConfig = signingConfigs.getByName("debug")
+
+            proguardFiles(
+            getDefaultProguardFile("proguard-android-optimize.txt"),
+            "proguard-rules.pro"
+        )
+        }
+    }
+}
+dependencies {
+    implementation("androidx.core:core-ktx:1.12.0")
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
+
+}
+
+flutter {
+    source = "../.."
+}
---
+++ android/app/proguard-rules.pro
@@ -0,0 +1,6 @@
+# Keep Hive classes
+-keep class io.hive.** { *; }
+-keep class no.nordicsemi.android.support.v18.** { *; }
+
+# Keep Flutter Local Notifications
+-keep class com.dexterous.flutterlocalnotifications.** { *; }
